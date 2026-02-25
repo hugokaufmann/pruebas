335b4fbdf6f7873a2dba2c166e10bbdd14efa797
Hugo Kaufmann <hugokaufmann@gmail.com>
2026-02-25 19:06:43 -0300

    Registrar app 'aplicacion' e incluir sus URLs

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 def inicio(request):
     return HttpResponse("Hola desde mi aplicación!")
+
+
+def hola(request):
+    return HttpResponse("Ahora tengo otra url sin parámetros")
 # Create your views here.
